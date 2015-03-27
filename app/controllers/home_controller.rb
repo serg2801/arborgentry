b370241d6077006586af41c4e0d744934118a11e
Divyang Hirpara <divyang@coglab.biz>
2015-03-27 07:28:20 +0530

    Set Login & registration page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
+  layout :resolve_layout
   def index
+  end
+
+  def login
+  end
+
+  def registration
+  end
+
+  private
+  def resolve_layout
+    case action_name
+    when "index"
+      "application"
+    else
+      "registration"
+    end
   end
 end
