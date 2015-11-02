6e7617b9f120a95f96f6607ef38d850c1e4f6d3a
jignesh <khokhanijignesh29@gmail.com>
2015-11-03 00:26:06 +0530

    remove images, dependent destroy for variant, option_type/value

--- app/models/option_type.rb
+++ app/models/option_type.rb
@@ -1,5 +1,5 @@
 class OptionType < ActiveRecord::Base
 	belongs_to :variant
-	has_many :option_values
+	has_many :option_values, dependent: :destroy
   accepts_nested_attributes_for :option_values, :allow_destroy => true, :reject_if => lambda { |a| a[:name].blank? }
 end
--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,5 +1,5 @@
 class Variant < ActiveRecord::Base
 	belongs_to :product
-	has_many :option_types
+	has_many :option_types, dependent: :destroy
   accepts_nested_attributes_for :option_types, :allow_destroy => true, :reject_if => lambda { |a| a[:name].blank? }
 end
