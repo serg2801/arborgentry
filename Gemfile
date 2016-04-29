990d03467265085203eddb936f8e45fdacc45f6f
Sergei Koretskiy <sergei.koretskiy@gmail.com>
2016-04-29 17:57:34 +0300

    add gem for production

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.1.5'
 gem 'rails', '4.1.8'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', group: :development
+gem 'rails_12factor', group: :production
 gem "pg", group: :production
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,7 +1,7 @@
 class Vendor < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable#, :confirmable
   
   has_many :categories, :through => :vendor_categories
   has_many :vendor_categories
