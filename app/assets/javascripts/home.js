a3a8be9b0c7edd1e53b21a7b889905c51b4fd2fd
Sergei Koretskiy <sergei.koretskiy@gmail.com>
2016-06-23 18:27:25 +0300

    add html for front end

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -10,8 +10,42 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require pace
+// require pace
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
-//= require script
+//= require shop_js/modernizr
+//= require shop_js/bootstrap
+//= require shop_js/bootstrap-hover-dropdown
+//= require shop_js/jquery.easing.1.3
+//= require shop_js/skrollr.min
+//= require shop_js/smooth-scroll
+
+//= require shop_js/jquery.appear
+//= require shop_js/wow.min
+//= require shop_js/page-scroll
+//= require shop_js/jquery.easypiechart
+//= require shop_js/jquery.parallax-1.1.3
+//= require shop_js/jquery.isotope.min
+//= require shop_js/owl.carousel.min
+//= require shop_js/jquery.magnific-popup.min
+//= require shop_js/popup-gallery
+//= require shop_js/text-effect
+//= require shop_js/jquery.tools.min
+//= require shop_js/jquery.revolution
+//= require shop_js/counter
+//= require shop_js/jquery.countTo
+//= require shop_js/jquery.fitvids
+//= require shop_js/imagesloaded.pkgd.min
+//= require shop_js/classie
+//= require shop_js/hamburger-menu
+//= require shop_js/main
+// require script
+
+// require shop_js/ajax-popup-slider
+// require shop_js/html5shiv
+// require shop_js/jquery.viewport.mini
+// require shop_js/one-page-main
+// require shop_js/velocity.min
+// require shop_js/velocity-animation
+
+
