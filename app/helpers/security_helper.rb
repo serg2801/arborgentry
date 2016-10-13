ad910bb5ae48525022de6586ebbc6f648c98db9d
VitaliiKhmelyk <vitaliikhmelyk@gmail.com>
2016-10-13 19:07:09 +0300

    Added spree permissions

--- app/helpers/security_helper.rb
+++ app/helpers/security_helper.rb
@@ -23,18 +23,18 @@ module SecurityHelper
         else
           actn_name = action_name  
         end  
-        if permission_key.nil?
+        if permission_key.blank?
           p_name = get_permission_name(contrlr_name, actn_name)
-          p = Permission.where("name = '#{p_name}' and (updated_at is not null)")      
+          p = Permission.where("(name = '#{p_name}') and (updated_at is not null)")      
         else
-          p = Permission.where("key = '#{permission_key}' and (updated_at is not null)")
+          p = Permission.where("(key = '#{permission_key}') and (updated_at is not null)")
         end
-        permission = (p.nil? || p.blank?)?nil:p[0].key
+        permission = (p.blank?)?nil:p[0].key
         res = permission.nil? || (@current_vendor.roles.map { |r| r.has_permission?(permission) }.include? true)
         if !res && permission_key.nil?  
            p_name = get_permission_name(contrlr_name, "all")
-           p = Permission.where("name = '#{p_name}' and (updated_at is not null)")   
-           permission = (p.nil? || p.blank?)?nil:p[0].key
+           p = Permission.where("(name = '#{p_name}') and (updated_at is not null)")   
+           permission = (p.blank?)?nil:p[0].key
            res = !(permission.nil?) && (@current_vendor.roles.map { |r| r.has_permission?(permission) }.include? true)
         end  
       end
@@ -66,8 +66,37 @@ module SecurityHelper
     vendor_not_authorized if !has_access_action(action_name)
   end  
 
-  def has_access_controller(controller_name, action_name)
+  def has_access_controller_action(controller_name, action_name)
     return check_access_ex(nil, controller_name, action_name)
+  end
+
+  def has_access_controller(controller_name)
+    res = false
+    unless current_vendor.blank? 
+      res = current_vendor.has_role? :admin
+      if !res
+        if controller_name.nil? || (controller_name.strip == "")
+          contrlr_name = params[:controller]
+        else
+          contrlr_name = controller_name
+        end  
+        actn_name = "any_action"
+        p_name = get_permission_name(contrlr_name, actn_name).gsub(actn_name, "")
+        all_p = Permission.where("(name like '%#{p_name}%') and (updated_at is not null)") 
+        unless all_p.blank?          
+          all_p.map do |p|
+            while !res do  
+              @current_vendor.roles.map do |r|
+                res = res || (r.has_permission?p.key)
+              end  
+            end
+          end
+        else
+          res = true    
+        end 
+      end
+    end
+    return res 
   end
 
 end
